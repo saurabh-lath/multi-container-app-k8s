96117a65b0dc0903f0d71b3e6be6b22841751e0c
Saurabh Lath <Saurabh.lath@kolmeo.com>
2021-10-16 13:01:33 +1100

    some refactoring changes

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker build -t saurabh333/multi-container-client:latest -t saurabh333/multi-container-client:$GIT_SHA -f ./client/Dockerfile ./client
-docker build -t saurabh333/multi-container-server:latest -t saurabh333/multi-container-server:$GIT_SHA -f ./server/Dockerfile ./server
-docker build -t saurabh333/multi-container-worker:latest -t saurabh333/multi-container-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+docker build -t saurabh333/multi-container-client:latest -t saurabh333/multi-container-client:$SHA -f ./client/Dockerfile ./client
+docker build -t saurabh333/multi-container-server:latest -t saurabh333/multi-container-server:$SHA -f ./server/Dockerfile ./server
+docker build -t saurabh333/multi-container-worker:latest -t saurabh333/multi-container-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push saurabh333/multi-container-client:latest
-docker push saurabh333/multi-container-client:$GIT_SHA
+docker push saurabh333/multi-container-client:$SHA
 docker push saurabh333/multi-container-server:latest
-docker push saurabh333/multi-container-server:$GIT_SHA
+docker push saurabh333/multi-container-server:$SHA
 docker push saurabh333/multi-container-worker:latest
-docker push saurabh333/multi-container-worker:$GIT_SHA
+docker push saurabh333/multi-container-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=saurabh333/multi-container-server:$GIT_SHA
-kubectl set image deployments/client-deployment client=saurabh333/multi-container-client:$GIT_SHA
-kubectl set image deployments/worker-deployment worker=saurabh333/multi-container-worker:$GIT_SHA
+kubectl set image deployments/server-deployment server=saurabh333/multi-container-server:$SHA
+kubectl set image deployments/client-deployment client=saurabh333/multi-container-client:$SHA
+kubectl set image deployments/worker-deployment worker=saurabh333/multi-container-worker:$SHA
